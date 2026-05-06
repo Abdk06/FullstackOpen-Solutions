78013e716d31d78e53422835a669f611b1f3c6f1
Abdk06 <abdulkader.alkadi@std.ankaramedipol.edu.tr>
2026-05-06 23:31:12 +0300

    Added solution for 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,15 @@ const morgan = require("morgan");
 
 app.use(express.json());
 
-app.use(morgan("tiny"));
+morgan.token("sentdata", (request, response) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :sentdata",
+  ),
+);
 
 let persons = [
   {
